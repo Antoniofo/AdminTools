b9217694940c400a11d7ac54558dff3759e638f8
Antoniofo <antonio57201@gmail.com>
2022-12-29 09:29:37 +0100

    initial commit

---
+++ AdminTools/AdminToolsPlugin.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Life;
+using Life.Network;
+using UnityEngine;
+
+namespace AdminTools
+{
+    public class AdminToolsPlugin : Plugin
+    {
+        public AdminToolsPlugin(IGameAPI api) : base(api)
+        {
+        }
+
+        public override void OnPluginInit()
+        {
+            base.OnPluginInit();
+
+
+            SChatCommand pos = new SChatCommand("/pos", "get pos", "/pos", async (player, args) =>
+            {
+                if (player.IsAdmin)
+                {
+                    await player.Save();
+                    player.SendText($"X:{player.character.LastPosX} Y:{player.character.LastPosY} Z:{player.character.LastPosZ}");
+                    Debug.Log($"X:{player.character.LastPosX} Y:{player.character.LastPosY} Z:{player.character.LastPosZ}");
+                }
+                else
+                {
+                    player.SendText($"Your not an admin");
+                }
+                
+
+            });
+
+            SChatCommand tp = new SChatCommand("/tp", "teleport to coordinate", "/tp x y z", (player, args) =>
+            {
+                if (player.IsAdmin)
+                {
+                    float x = 0;
+                    float y = 0;
+                    float z = 0;
+                    try
+                    {
+                        x = float.Parse(args[0]);
+                        y = float.Parse(args[1]);
+                        z = float.Parse(args[2]);
+                    }
+                    catch {
+                        player.SendText("Error parsing ");
+                        return;
+                    }
+
+                    player.setup.TargetSetPosition(new Vector3(x,y,z));
+                    player.SendText($"X:{x} Y:{y} Z:{z}");
+                }
+                else
+                {
+                    player.SendText($"Your not an admin");
+                }
+
+
+            });
+            tp.Register();
+            pos.Register();
+        }
+
+        
+
+    }
+}
